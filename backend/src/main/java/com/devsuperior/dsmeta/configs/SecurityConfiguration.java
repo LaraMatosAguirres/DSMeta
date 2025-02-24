7c51e91052f4a79fd5b10fa73507df40526a6e25
LaraMatosAguirres <lara.laramatos@gmail.com>
2025-02-24 14:57:55 -0300

    007 - Axios, userEffect and CORS Configuration

--- backend/src/main/java/com/devsuperior/dsmeta/configs/SecurityConfiguration.java
+++ backend/src/main/java/com/devsuperior/dsmeta/configs/SecurityConfiguration.java
@@ -1,9 +1,15 @@
 package com.devsuperior.dsmeta.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -11,7 +17,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .cors() // Habilita o CORS
+                .and()
+                .authorizeRequests()
                 .antMatchers("/h2/**", "/report-sales/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -21,6 +30,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .headers().frameOptions().sameOrigin();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5173")); // Defina sua origem
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
-
-
