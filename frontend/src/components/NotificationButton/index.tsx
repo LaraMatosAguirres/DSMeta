02fd76c8a39df77a3d679f58a3062b1d6e423b1d
LaraMatosAguirres <lara.laramatos@gmail.com>
2025-03-05 19:42:14 -0300

    Notification update

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,9 +1,22 @@
+import axios from "axios";
 import AvatarIcon from "../../assets/img/avatar-icon 1.svg";
 import "./styles.css";
 
-function NotificationButton() {
+type Props = {
+    saleId: number;
+}
+
+function handleClick(id : number){
+  axios('${BASE_URL}/report-sales/${id}/notification')
+    .then(response => {
+      console.log("sucesso")
+    })
+
+}
+
+function NotificationButton({saleId} : Props) {
   return (
-    <div className="dsmeta-red-btn">
+    <div className="dsmeta-red-btn" onClick={() => handleClick(saleId)}>
       <img src={AvatarIcon} alt="Notificar" />
     </div>
   );
