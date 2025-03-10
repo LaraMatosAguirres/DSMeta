7dd9a057c1008eec8daf660de82ab73e1fea3c05
LaraMatosAguirres <lara.laramatos@gmail.com>
2025-03-10 14:57:44 -0300

    Images

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -9,7 +9,7 @@ function Header() {
         <h1>DSMeta</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          <a href="https://www.linkedin.com/in/lara-matos-aguirres-3a632b244/"> Lara Matos Aguirres</a>
         </p>
       </div>
     </header>
